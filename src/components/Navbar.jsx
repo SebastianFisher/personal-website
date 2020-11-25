365ace44bfdf528110ac0c63a7c670f4bf48bcea
SebastianF <sbfisher01@yahoo.com>
2020-11-25 11:45:57 -0800

    finished navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,15 @@ import onClickOutside from 'react-onclickoutside';
 import "./Navbar.css";
 import { Link } from 'react-router-dom';
 
-
 class Navbar extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            nav: ''
+            nav: '',
+            links: ["home", "about", "projects", "contact"],
+            animation: '',
+            burgerToggle: ''
         }
 
         this.toggleNav = this.toggleNav.bind(this);
@@ -17,34 +19,57 @@ class Navbar extends React.Component {
     }
 
     toggleNav() {
-        this.setState(state => ({ nav: state.nav === '' ? ' nav-active' : '' }));
+        this.setState(state => {
+            let nav = '';
+            let animation = '';
+            let burgerToggle = '';
+            if (state.nav === '') {
+                nav = 'nav-active';
+                animation = 'in';
+                burgerToggle = 'toggle';
+            }
+            return {
+                nav,
+                animation,
+                burgerToggle
+            }
+        });
     }
 
     handleClickOutside() {
-        this.toggleNav();
+        this.setState({ nav: '', animation: '', burgerToggle: '' });
     }
 
     render() {
+        const links = this.state.links.map((link, index) => {
+            const numLinks = this.state.links.length;
+            let animation;
+            if (this.state.animation === 'in') {
+                animation = `navIn 0.5s ease forwards ${index / (numLinks + 4) + 0.3}s`;
+            }
+            let destination;
+            if (link === 'home') {
+                destination = './';
+            } else {
+                destination = `./${link}`;
+            }
+
+            return (
+                <Link to={destination}>
+                    <li key={link} style={{ animation }}>{link}</li>
+                </Link>
+            )
+        });
+
         return (
             <div className="nav">
                 <div className="logo">
                     Sebastian Fisher
                 </div>
-                <ul className={`nav-links${this.state.nav}`}>
-                    <Link to="./">
-                        <li>Home</li>
-                    </Link>
-                    <Link to="./about">
-                        <li>About</li>
-                    </Link>
-                    <Link to="./projects">
-                        <li>Projects</li>
-                    </Link>
-                    <Link to="./contact">
-                        <li>Contact</li>
-                    </Link>
+                <ul className={`nav-links ${this.state.nav}`}>
+                    {links}
                 </ul>
-                <div className="burger" onClick={this.toggleNav}>
+                <div className={`burger ${this.state.burgerToggle}`} onClick={this.toggleNav}>
                     <div id="line1"></div>
                     <div id="line2"></div>
                     <div id="line3"></div>
